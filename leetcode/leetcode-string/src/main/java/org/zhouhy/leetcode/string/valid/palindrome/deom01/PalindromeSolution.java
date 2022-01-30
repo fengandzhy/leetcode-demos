02b260d56133186e44d9060d162ba0edbf5af819
fengandzhy <fengandzhy@gmail.com>
2022-01-30 18:34:46 +1300

    palindrome

---
+++ leetcode/leetcode-string/src/main/java/org/zhouhy/leetcode/string/valid/palindrome/deom01/PalindromeSolution.java
@@ -0,0 +1,60 @@
+package org.zhouhy.leetcode.string.valid.palindrome.deom01;
+
+public class PalindromeSolution {
+
+    @SuppressWarnings("UnnecessaryLabelOnBreakStatement")
+    public boolean isPalindrome(String s) {
+        int begin = 0;
+        int end = s.length() - 1;
+        lableA:
+        while (true) {            
+            char cBegin = s.charAt(begin);
+            char cEnd = s.charAt(end);
+            if(isBreak(begin,end,s.length())) {
+                break;
+            }
+            while (willGoHead(cBegin)) {
+                begin++;
+                if(isBreak(begin,end,s.length())) {
+                    break lableA;
+                }
+                cBegin = s.charAt(begin);
+            }
+            while (willGoHead(cEnd)) {
+                end--;
+                if(isBreak(begin,end,s.length())) {
+                    break lableA;
+                }
+                cEnd = s.charAt(end);
+            }
+            if (Character.toLowerCase(cBegin) == Character.toLowerCase(cEnd)) {
+                if(isBreak(begin,end,s.length())){
+                    break lableA;
+                }else{
+                    begin++;
+                    end--;
+                    if(isBreak(begin,end,s.length())){
+                        break lableA;
+                    }
+                }                
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isBreak(int begin, int end,int size){
+        return begin == end || begin >= size || end <= 0;
+    }
+
+    private boolean willGoHead(char c) {
+        if ((int) c >= 65 && (int) c <= 90) {
+            return false;
+        }
+        if ((int) c >= 97 && (int) c <= 122) {
+            return false;
+        }
+        return (int) c < 48 || (int) c > 57;
+    }
+}
---
+++ leetcode/leetcode-string/src/main/java/org/zhouhy/leetcode/string/valid/palindrome/deom01/Test.java
@@ -0,0 +1,9 @@
+package org.zhouhy.leetcode.string.valid.palindrome.deom01;
+
+public class Test {
+    public static void main(String[] args) {
+        PalindromeSolution solution = new PalindromeSolution();
+        String str = "A man, a plan, a canal: Panama";
+        System.out.println(solution.isPalindrome(str));
+    }
+}
