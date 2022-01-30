f7cd14117283f15ab68b795d98c513c1657763c0
fengandzhy <fengandzhy@gmail.com>
2022-01-31 06:02:52 +1300

    PalindromeSolution

--- leetcode/leetcode-string/src/main/java/org/zhouhy/leetcode/string/valid/palindrome/deom02/PalindromeSolution.java
+++ leetcode/leetcode-string/src/main/java/org/zhouhy/leetcode/string/valid/palindrome/deom02/PalindromeSolution.java
@@ -3,7 +3,29 @@ package org.zhouhy.leetcode.string.valid.palindrome.deom02;
 public class PalindromeSolution {
 
     public boolean isPalindrome(String s) {
-        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if((int) c >= 65 && (int) c <= 90){                
+                sb.append((char)(c+32));                
+            }
+            if((int) c >= 48 && (int) c <= 57){
+               sb.append(c);                
+            }
+            if ((int) c >= 97 && (int) c <= 122) {
+                sb.append(c);                
+            }
+        }
+        String str = sb.toString();        
+        int length = str.length();
+        for(int i=0;i<length;i++){
+            if(i==length-1-i){
+                break;
+            }
+            if(str.charAt(i) != str.charAt(length-1-i)){
+                return false;
+            }            
+        }        
         return true;
     }
 }
---
+++ leetcode/leetcode-string/src/main/java/org/zhouhy/leetcode/string/valid/palindrome/deom02/Test.java
@@ -0,0 +1,9 @@
+package org.zhouhy.leetcode.string.valid.palindrome.deom02;
+
+public class Test {
+    public static void main(String[] args) {
+        PalindromeSolution solution = new PalindromeSolution();
+        System.out.println(solution.isPalindrome("0P"));
+        System.out.println((int)'9');
+    }
+}
