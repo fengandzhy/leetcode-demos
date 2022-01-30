376441ab460dfa9da7fce797ee7c755fcb5f4360
fengandzhy <fengandzhy@gmail.com>
2022-01-31 04:56:09 +1300

    PalindromeSolution

---
+++ leetcode/leetcode-string/src/main/java/org/zhouhy/leetcode/string/valid/palindrome/deom02/PalindromeSolution.java
@@ -0,0 +1,9 @@
+package org.zhouhy.leetcode.string.valid.palindrome.deom02;
+
+public class PalindromeSolution {
+
+    public boolean isPalindrome(String s) {
+        
+        return true;
+    }
+}
