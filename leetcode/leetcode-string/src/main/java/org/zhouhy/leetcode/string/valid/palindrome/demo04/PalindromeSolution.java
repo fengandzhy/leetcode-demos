74ec4303ddcf7191679ba755d193656150ced830
fengandzhy <fengandzhy@gmail.com>
2022-02-17 21:25:12 +1300

    solution 4

---
+++ leetcode/leetcode-string/src/main/java/org/zhouhy/leetcode/string/valid/palindrome/demo04/PalindromeSolution.java
@@ -0,0 +1,22 @@
+package org.zhouhy.leetcode.string.valid.palindrome.demo04;
+
+
+public class PalindromeSolution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        StringBuffer sb = new StringBuffer();
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(Character.isLetterOrDigit(c)){
+                sb.append(c);
+            }
+        }
+        s = sb.toString();
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i)!=s.charAt(s.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }    
+}
---
+++ leetcode/leetcode-string/src/main/java/org/zhouhy/leetcode/string/valid/palindrome/demo04/Test.java
@@ -0,0 +1,10 @@
+package org.zhouhy.leetcode.string.valid.palindrome.demo04;
+
+
+public class Test {
+    public static void main(String[] args) {
+        PalindromeSolution solution = new PalindromeSolution();
+        String str = "a.";
+        System.out.println(solution.isPalindrome(str));
+    }    
+}
