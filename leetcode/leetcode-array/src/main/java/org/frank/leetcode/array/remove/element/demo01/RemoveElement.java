d31386a58994d397414c1ffcdf19a0f66b79dfd0
fengandzhy <fengandzhy@gmail.com>
2023-08-30 09:56:28 +1200

    leetcode-array most water

---
+++ leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/remove/element/demo01/RemoveElement.java
@@ -0,0 +1,24 @@
+package org.frank.leetcode.array.remove.element.demo01;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        List<Integer> tempList = new ArrayList<>();
+        for(int num:nums){
+            tempList.add(num);
+        }
+        Iterator<Integer> iter = tempList.iterator();
+        while(iter.hasNext()){
+            if(val == iter.next()){
+                iter.remove();
+            }
+        }        
+        for(int i = 0;i<tempList.size();i++){
+            nums[i] = tempList.get(i);
+        }
+        return tempList.size();
+    }
+}
