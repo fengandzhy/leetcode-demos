277f47636a417144f168e809fdb2d0859e46d382
fengandzhy <fengandzhy@gmail.com>
2023-09-08 09:29:46 +1200

    leetcode-depth-first same tree

--- leetcode/leetcode-string/src/main/java/org/frank/leetcode/string/roman/to/integer/demo01/RomanToIntegerSolution.java
+++ leetcode/leetcode-string/src/main/java/org/frank/leetcode/string/roman/to/integer/demo01/RomanToIntegerSolution.java
@@ -6,11 +6,10 @@ public class RomanToIntegerSolution {
      * https://leetcode.cn/problems/roman-to-integer
      * 通常情况下，罗马数字中小的数字在大的数字的右边。若输入的字符串满足该情况，那么可以将每个字符视作一个单独的值，累加每个字符对应的数值即可。
      *
-     * 例如 XXVII\texttt{XXVII}XXVII 可视作 X+X+V+I+I=10+10+5+1+1=27\texttt{X}+\texttt{X}+\texttt{V}+\texttt{I}+\texttt{I}=10+10+5+1+1=27X+X+V+I+I=10+10+5+1+1=27。
-     *
+     * 例如 XXVII 可视作 X + X + V + I + I = 10 + 10 + 5 + 1 + 1
      * 若存在小的数字在大的数字的左边的情况，根据规则需要减去小的数字。对于这种情况，我们也可以将每个字符视作一个单独的值，若一个数字右侧的数字比它大，则将该数字的符号取反。
      *
-     * 例如 XIV\texttt{XIV}XIV 可视作 X−I+V=10−1+5=14\texttt{X}-\texttt{I}+\texttt{V}=10-1+5=14X−I+V=10−1+5=14。
+     * 例如 XIV 可视作 X - I + V = 14
      * 
      *     
      * */
