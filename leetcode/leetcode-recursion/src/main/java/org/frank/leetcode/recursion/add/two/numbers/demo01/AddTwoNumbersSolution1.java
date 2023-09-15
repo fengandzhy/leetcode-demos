a13b71ae71c4c36f4be8933285076bb55cbd3914
fengandzhy <fengandzhy@gmail.com>
2023-09-16 07:50:08 +1200

    recursion AddTwoNumbersSolution1

--- leetcode/leetcode-recursion/src/main/java/org/frank/leetcode/recursion/add/two/numbers/demo01/AddTwoNumbersSolution1.java
+++ leetcode/leetcode-recursion/src/main/java/org/frank/leetcode/recursion/add/two/numbers/demo01/AddTwoNumbersSolution1.java
@@ -1,4 +1,7 @@
 package org.frank.leetcode.recursion.add.two.numbers.demo01;
 
 public class AddTwoNumbersSolution1 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
 }
