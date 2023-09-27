627f4e7552f254cd38fedd7030be568e633acc40
fengandzhy <fengandzhy@gmail.com>
2023-09-28 10:27:28 +1300

    leetcode-array SearchInsertPositionSolution1

---
+++ leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/search/insert/position/demo01/SearchInsertPositionSolution1.java
@@ -0,0 +1,20 @@
+package org.frank.leetcode.array.search.insert.position.demo01;
+
+/**
+ * https://leetcode.cn/problems/search-insert-position/
+ * 
+ * */
+public class SearchInsertPositionSolution1 {
+    public int searchInsert(int[] nums, int target) {
+        int index = 0;
+        if(nums!=null && nums.length>0){            
+            for(int i = 0; i< nums.length; i++){
+                if(nums[i] >= target){
+                    return i;
+                }
+            }
+            index = nums.length;
+        }
+        return index;
+    }
+}
