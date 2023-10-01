ec68bb4ddd9be579806b9d4e5d3a29a8293379c9
fengandzhy <fengandzhy@gmail.com>
2023-10-01 15:31:39 +1300

    leetcode-array CountItemsMatchingARuleSolution1

--- leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/count/items/matching/a/rule/demo01/CountItemsMatchingARuleSolution1.java
+++ leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/count/items/matching/a/rule/demo01/CountItemsMatchingARuleSolution1.java
@@ -1,6 +1,31 @@
 package org.frank.leetcode.array.count.items.matching.a.rule.demo01;
 
+import java.util.List;
+
 public class CountItemsMatchingARuleSolution1 {
-    
-    
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int itemCount = 0;
+        if(ruleKey.equals("type")){
+            for(List<String> prods:items){
+                if(prods.get(0).equals(ruleValue)){
+                    itemCount++;
+                }
+            }
+        }
+        if(ruleKey.equals("color")){
+            for(List<String> prods:items){
+                if(prods.get(1).equals(ruleValue)){
+                    itemCount++;
+                }
+            }
+        }
+        if(ruleKey.equals("name")){
+            for(List<String> prods:items){
+                if(prods.get(2).equals(ruleValue)){
+                    itemCount++;
+                }
+            }
+        }
+        return itemCount;
+    }    
 }
