8f4e49711cc1b31beb6d2b2e73e45b2a8af95449
fengandzhy <fengandzhy@gmail.com>
2023-10-02 22:12:36 +1300

    leetcode-sorted MajorityElementSolution1

--- leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/first/missing/positive/demo01/FirstMissingPositiveSolution1.java
+++ leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/first/missing/positive/demo01/FirstMissingPositiveSolution1.java
@@ -4,6 +4,9 @@ package org.frank.leetcode.array.first.missing.positive.demo01;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * https://leetcode.cn/problems/first-missing-positive/
+ * */
 public class FirstMissingPositiveSolution1 {
     public int firstMissingPositive(int[] nums) {
         List<Integer> list = Arrays.stream(nums).boxed().sorted().collect(Collectors.toList());
@@ -20,21 +23,23 @@ public class FirstMissingPositiveSolution1 {
         if(list.get(list.size() -1) < 1){
             return 1;
         }
-        int[] signs = new int[list.get(list.size()-1)];        
         for (int i = 0; i < list.size(); i++) {
             int currentInt = list.get(i);
-            if(currentInt > 0){
-                signs[currentInt-1] = 1;                
+            if(currentInt - prevInt >=2){
+                if(currentInt < 2){
+                    prevInt = currentInt;                    
+                }else {
+                    if(prevInt > 0){
+                        return prevInt + 1;
+                    }else{
+                        return 1;
+                    }
+                }
+            }else{
+                prevInt = currentInt;
             }
         }
-        int index = 0;
-        for(int i = 0; i< signs.length; i ++){
-            index = i+1;
-            if(signs[i] == 0){
-                return i+1;
-            }
-        }
-        return index + 1;
+        return prevInt + 1;
     }
 
     public static void main(String[] args) {
