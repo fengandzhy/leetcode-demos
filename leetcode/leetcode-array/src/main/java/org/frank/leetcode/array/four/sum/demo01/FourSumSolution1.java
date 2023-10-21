6ea69e5aec7705342bd9b3583fd8235b9737f897
fengandzhy <fengandzhy@gmail.com>
2023-10-22 12:25:54 +1300

    leetcode-sorted MajorityElementSolution1

--- leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/four/sum/demo01/FourSumSolution1.java
+++ leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/four/sum/demo01/FourSumSolution1.java
@@ -1,73 +1,80 @@
 package org.frank.leetcode.array.four.sum.demo01;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * https://leetcode.cn/problems/4sum/description/
- * */
+ */
 
 public class FourSumSolution1 {
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> list = new ArrayList<>();
         int length = nums.length;
-        if(length < 4){
+        if (length < 4) {
             return list;
         }
-        for(int i = 0; i < length; i++){
-            List<Integer> tempList = new ArrayList<>();
-            boolean isFound = false;
-            int first = nums[i];            
-            int tempTarget1 = target - first;
-            for(int j = i + 1; j < length; j ++){
-                int second = nums[j];                
-                int tempTarget2 = tempTarget1 - second;
-                for(int m = j + 1; m < length; m ++){
-                    int third = nums[m];                    
-                    int tempTarget3 = tempTarget2 - third;
-                    for(int n = m+1; n < length; n ++){
-                        int forth = nums[n];
-                        if(forth == tempTarget3){
-                            tempList.add(first);
-                            tempList.add(second);
-                            tempList.add(third);
-                            tempList.add(forth);
-                            isFound = true;
+        Arrays.sort(nums);
+        if ((long)nums[length - 4] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+            return list;
+        }
+        if ((long)nums[0] + nums[1] + nums[2] + nums[3] > target) {
+            return list;
+        }
+        for (int i = 0; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int first = nums[i];
+            if ((long)nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target) {
+                break;
+            }
+            if ((long)first + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+                continue;
+            }
+            for (int j = i + 1; j < length - 2; j++) {
+                if(j > i + 1 && nums[j] == nums[j - 1]){
+                    continue;
+                }
+                int second = nums[j];
+                if((long)first + second + nums[j + 1] + nums[j + 2] > target ){
+                    break;
+                }
+                if((long)first + second + nums[length - 2] + nums[length - 1] < target){
+                    continue;
+                }
+                int left = j + 1;
+                int right = length - 1;
+                while(left < right){
+                    long sum = (long)first + second + nums[left] + nums[right];
+                    if(sum == target){
+                        list.add(Arrays.asList(first,second,nums[left],nums[right]));
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
                         }
-                        if(isFound){
-                            break;
+                        right--;
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
                         }
-                    }
-                    if(isFound){
-                        break;
+                        left++;
+                    } else if (sum < target) {
+                        left++;
+                    }else{
+                        right--;
                     }
                 }
-                if(isFound){
-                    break;
-                }                
-            }
-            tempList.sort((a, b)-> {
-                return a.compareTo(b);
-            });
-            if(!isExist(list,tempList )){
-                list.add(tempList);
             }
         }
         return list;
     }
-    
-    private boolean isExist(List<List<Integer>> list, List<Integer> tempList ){
-        if(list.size() == 0){
-            return false;
-        }
-        for(List<Integer> integers: list){
-            for(int i = 0; i < tempList.size(); i ++){
-                if(integers.get(i) != tempList.get(i)){
-                    return false;
-                }
-            }
-        }
-        return true;
+
+    public static void main(String[] args) {
+        FourSumSolution1 solution1 = new FourSumSolution1();
+        solution1.fourSum(new int[]{0,0,0,1000000000,1000000000,1000000000,1000000000}, 1000000000);
     }
 }
+
+
+
