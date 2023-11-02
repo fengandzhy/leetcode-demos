d9d42bab42b13530c8d26c0f9b1bb24ac7e65df5
fengandzhy <fengandzhy@gmail.com>
2023-11-02 22:29:28 +1300

    leetcode-sorted MajorityElementSolution1

---
+++ leetcode/leetcode-linked_list/src/main/java/org/frank/leetcode/linked/list/linked/list/cycle/demo01/LinkedListCycleSolution1.java
@@ -0,0 +1,31 @@
+package org.frank.leetcode.linked.list.linked.list.cycle.demo01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.cn/problems/linked-list-cycle/
+ * */
+public class LinkedListCycleSolution1 {
+    public boolean hasCycle(ListNode head) {
+        boolean isCycle = false;
+        if(head == null){
+            return false;
+        }
+        ListNode node = head;
+        List<ListNode> nodeList = new ArrayList<>();
+        nodeList.add(node);
+        while(true){
+            node = node.next;
+            if(node == null){
+                break;
+            }
+            if(nodeList.contains(node)){
+                isCycle = true;
+                break;
+            }
+            nodeList.add(node);
+        }
+        return isCycle;
+    }
+}
