fd766b0fca510f03cfbd85a6b2a8ec76a89f7009
fengandzhy <fengandzhy@gmail.com>
2023-11-15 07:44:50 +1300

    leetcode-linked_list MergeTwoSortedListsSolutions1

--- leetcode/leetcode-linked_list/src/main/java/org/frank/leetcode/linked/list/merge/two/sorted/lists/demo01/MergeTwoSortedListsSolutions1.java
+++ leetcode/leetcode-linked_list/src/main/java/org/frank/leetcode/linked/list/merge/two/sorted/lists/demo01/MergeTwoSortedListsSolutions1.java
@@ -50,23 +50,5 @@ public class MergeTwoSortedListsSolutions1 {
             currentNode = currentNode.next;
         }        
         return heardNode;
-    }
-
-    public static void main(String[] args) {
-        MergeTwoSortedListsSolutions1 solutions1 = new MergeTwoSortedListsSolutions1();
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(4);
-        node1.next = node2;
-        node2.next = node3;
-
-        ListNode node4 = new ListNode(1);
-        ListNode node5 = new ListNode(3);
-        ListNode node6 = new ListNode(4);
-        node4.next = node5;
-        node5.next = node6;
-
-        solutions1.mergeTwoLists(node1,node4);
-        
-    }
+    }    
 }
