3164354d7bd3e9adb23ec4057ab315c2266bf8ea
fengandzhy <fengandzhy@gmail.com>
2023-11-15 06:55:18 +1300

    leetcode-linked_list MergeTwoSortedListsSolutions1

--- leetcode/leetcode-linked_list/src/main/java/org/frank/leetcode/linked/list/merge/two/sorted/lists/demo01/MergeTwoSortedListsSolutions1.java
+++ leetcode/leetcode-linked_list/src/main/java/org/frank/leetcode/linked/list/merge/two/sorted/lists/demo01/MergeTwoSortedListsSolutions1.java
@@ -5,21 +5,51 @@ package org.frank.leetcode.linked.list.merge.two.sorted.lists.demo01;
  * */
 public class MergeTwoSortedListsSolutions1 {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null && list2 == null){
-            return null;
-        }
-        if(list2 == null){
-            return list1;
-        }
-        if(list1 == null){
-            return list2;
-        }        
-        while(true){
-            
+        ListNode heardNode = null;
+//        ListNode currentNode = null;
+//        ListNode previousNode = null;
+        while(true){                        
             if(list1 == null && list2 == null){
                 break;
             }
+            if(list2 == null){
+                heardNode = list1;
+//                if(heardNode == null){
+//                    heardNode = list1;
+//                }
+                break;
+            }
+            if(list1 == null){
+                heardNode = list2;
+//                if(heardNode == null){
+//                   heardNode = list2; 
+//                }
+                break;
+            }
+            if(list1.val < list2.val){
+                if(heardNode == null){
+                    heardNode = list1;
+                }
+//                if(currentNode == null){
+//                    currentNode = list1;
+//                }
+//                ListNode tempNode = currentNode;
+//                currentNode = list1;
+//                tempNode.next = currentNode;
+                list1 = list1.next;
+            }else {
+                if(heardNode == null){
+                    heardNode = list2;
+                }
+//                if(currentNode == null){
+//                    currentNode = list2;
+//                }
+//                ListNode tempNode = currentNode;
+//                currentNode = list2;
+//                tempNode.next = currentNode;
+                list2 = list2.next;
+            }
         }
-        return null;
-    }
+        return heardNode;
+    }   
 }
