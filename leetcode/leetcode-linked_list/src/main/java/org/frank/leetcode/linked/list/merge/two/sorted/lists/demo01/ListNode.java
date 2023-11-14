ba9e66d3134a4ba02dff97e69dc6d66a273d9618
fengandzhy <fengandzhy@gmail.com>
2023-11-14 22:12:17 +1300

    leetcode-linked_list

---
+++ leetcode/leetcode-linked_list/src/main/java/org/frank/leetcode/linked/list/merge/two/sorted/lists/demo01/ListNode.java
@@ -0,0 +1,13 @@
+package org.frank.leetcode.linked.list.merge.two.sorted.lists.demo01;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { 
+        this.val = val; 
+    }
+    ListNode(int val, ListNode next) { 
+        this.val = val; this.next = next; 
+    }
+}
