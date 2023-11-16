37b62a24796781e9dc3284763fc2d61913c64a48
fengandzhy <fengandzhy@gmail.com>
2023-11-17 07:29:26 +1300

    leetcode-linked_list MergeTwoSortedListsSolutions1

--- leetcode/leetcode-linked_list/src/main/java/org/frank/leetcode/linked/list/merge/two/sorted/lists/demo3/MergeTwoSortedListsSolutions3.java
+++ leetcode/leetcode-linked_list/src/main/java/org/frank/leetcode/linked/list/merge/two/sorted/lists/demo3/MergeTwoSortedListsSolutions3.java
@@ -4,7 +4,8 @@ import org.frank.leetcode.linked.list.merge.two.sorted.lists.ListNode;
 
 public class MergeTwoSortedListsSolutions3 {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode listNode = new ListNode(-1);
+        ListNode headerNode = new ListNode(-1);
+        ListNode listNode = headerNode;
         if(list1 == null){
             return list2;
         }
@@ -25,12 +26,13 @@ public class MergeTwoSortedListsSolutions3 {
             }
             if(list1.val < list2.val){
                 listNode.next = list1;
-                list1 = list1.next;
+                list1 = list1.next;                
             }else{
                 listNode.next = list2;
-                list2 = list2.next;
+                list2 = list2.next;                
             }
+            listNode = listNode.next;
         }
-        return listNode.next;
+        return headerNode.next;
     }
 }
