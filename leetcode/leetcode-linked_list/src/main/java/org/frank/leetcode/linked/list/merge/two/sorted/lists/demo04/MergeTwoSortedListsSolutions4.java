c80a749b7d26cff2992e75e79a5cedc5c5a593da
fengandzhy <fengandzhy@gmail.com>
2023-11-17 07:49:16 +1300

    leetcode-linked_list MergeTwoSortedListsSolutions1

--- leetcode/leetcode-linked_list/src/main/java/org/frank/leetcode/linked/list/merge/two/sorted/lists/demo04/MergeTwoSortedListsSolutions4.java
+++ leetcode/leetcode-linked_list/src/main/java/org/frank/leetcode/linked/list/merge/two/sorted/lists/demo04/MergeTwoSortedListsSolutions4.java
@@ -1,6 +1,7 @@
 package org.frank.leetcode.linked.list.merge.two.sorted.lists.demo04;
 
 import org.frank.leetcode.linked.list.merge.two.sorted.lists.ListNode;
+import org.frank.leetcode.linked.list.merge.two.sorted.lists.demo01.MergeTwoSortedListsSolutions1;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,12 +22,10 @@ public class MergeTwoSortedListsSolutions4 {
             if(list1 == null){
                 nodes.add(list2);
                 list2 = list2.next;
-            }
-            if(list2 == null){
+            }else if(list2 == null){
                 nodes.add(list1);
                 list1 = list1.next;
-            }
-            if(list1.val < list2.val){
+            }else if(list1.val < list2.val){
                 nodes.add(list1);
                 list1 = list1.next;
             }else{
@@ -38,5 +37,21 @@ public class MergeTwoSortedListsSolutions4 {
             nodes.get(i-1).next = nodes.get(i);
         }
         return nodes.get(0);
+    }
+
+    public static void main(String[] args) {
+        MergeTwoSortedListsSolutions4 solutions4 = new MergeTwoSortedListsSolutions4();
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+
+        ListNode node4 = new ListNode(1);
+        ListNode node5 = new ListNode(3);
+        ListNode node6 = new ListNode(4);
+        node4.next = node5;
+        node5.next = node6;
+        solutions4.mergeTwoLists(node1,node4);
     }
 }
