b370416e98e23614a814de89c2dfc64e2f689146
fengandzhy <fengandzhy@gmail.com>
2023-11-18 22:39:17 +1300

    leetcode-linked_list MergeKSortedListsSolutions1

--- leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/plus/one/demo02/PlusOneSolution2.java
+++ leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/plus/one/demo02/PlusOneSolution2.java
@@ -2,6 +2,27 @@ package org.frank.leetcode.array.plus.one.demo02;
 
 public class PlusOneSolution2 {
     public int[] plusOne(int[] digits) {
-        return null;
+        boolean isAllZero = false;
+        int length = digits.length;
+        for(int i = length -1; i >= 0; i--){
+            if(digits[i] != 9){
+                digits[i] = digits[i] + 1;
+                break;
+            }else{
+                digits[i] = 0;
+                if(i == 0){
+                   isAllZero = true; 
+                }
+            }
+        }
+        if(isAllZero){
+            int[] newArray = new int[length + 1];
+            newArray[0] = 1;
+            for(int i = 1;i < length + 1; i ++){
+                newArray[i] = 0;
+            }
+            return newArray;
+        }
+        return digits;
     }
 }
