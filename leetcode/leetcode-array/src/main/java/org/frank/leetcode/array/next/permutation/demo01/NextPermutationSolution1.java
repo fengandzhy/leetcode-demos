9900cd4f71951631a495e2b5cc409af254096513
fengandzhy <fengandzhy@gmail.com>
2023-11-19 16:36:09 +1300

    leetcode-linked_list MergeKSortedListsSolutions1

--- leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/next/permutation/demo01/NextPermutationSolution1.java
+++ leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/next/permutation/demo01/NextPermutationSolution1.java
@@ -1,5 +1,9 @@
 package org.frank.leetcode.array.next.permutation.demo01;
 
+
+/**
+ * https://leetcode.cn/problems/next-permutation/description/
+ * */
 public class NextPermutationSolution1 {
 
     public void nextPermutation(int[] nums) {
