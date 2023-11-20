e07f7c9a095ade357b233a65fda5d808ea751196
fengandzhy <fengandzhy@gmail.com>
2023-11-20 22:15:32 +1300

    leetcode-linked_list MergeKSortedListsSolutions1

--- leetcode/leetcode-string/src/main/java/org/frank/leetcode/string/count/and/say/demo01/CountAndSaySolution1.java
+++ leetcode/leetcode-string/src/main/java/org/frank/leetcode/string/count/and/say/demo01/CountAndSaySolution1.java
@@ -1,11 +1,10 @@
 package org.frank.leetcode.string.count.and.say.demo01;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 public class CountAndSaySolution1 {
     public String countAndSay(int n) {
-        int count = 0;
+        int count = 1;
         String str = "1";
         return calculate(n,count,str);
     }
@@ -21,19 +20,31 @@ public class CountAndSaySolution1 {
     
     private String generateString(String str){
         StringBuilder sb = new StringBuilder();
-        Map<String,Integer> map = new HashMap<>();
-        for(int i = 0; i<str.length()-2;i++){
-            String temp = str.substring(i,i+1);
-            if(map.containsKey(temp)){
-                map.put(temp, map.get(temp)+1);
-            }else{
-                map.put(temp,1);
+        char[] chars = str.toCharArray();
+        int count = 1;
+        int left = 0;
+        int right = 1;
+        while(true){
+            if(right > str.length() - 1){
+                sb.append(count);
+                sb.append(chars[left]);
+                break;
             }
-        }
-        for(Map.Entry<String,Integer> entry:map.entrySet()){
-            sb.append(entry.getValue());
-            sb.append(entry.getKey());
-        }
+            if(chars[left]==(chars[right])){
+                count ++;
+            }else{
+                sb.append(count);
+                sb.append(chars[left]);
+                count = 1;
+            }
+            left = left + 1;
+            right = right + 1;
+        }        
         return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        CountAndSaySolution1 solution1 = new CountAndSaySolution1();
+        solution1.countAndSay(4);
     }
 }
