2ef999647608fc618d766729a12c930f19020854
fengandzhy <fengandzhy@gmail.com>
2023-11-21 21:07:18 +1300

    leetcode-linked_list MergeKSortedListsSolutions1

--- leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/sudoku/solver/demo01/SudokuSolverSolution1.java
+++ leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/sudoku/solver/demo01/SudokuSolverSolution1.java
@@ -1,7 +1,15 @@
 package org.frank.leetcode.array.sudoku.solver.demo01;
 
+/**
+ * https://leetcode.cn/problems/sudoku-solver/description/
+ * */
 public class SudokuSolverSolution1 {
     public void solveSudoku(char[][] board) {
+        int rows[][] = new int[9][9];
+        int columns[][] = new int[9][9];
+        int subBox[][][] = new int[3][3][9];
+        boolean valid = false;
+        
         
     }
 }
