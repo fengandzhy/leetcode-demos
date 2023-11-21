c3c2318a5702a988411e046756308f44b35713db
fengandzhy <fengandzhy@gmail.com>
2023-11-21 21:31:11 +1300

    leetcode-linked_list MergeKSortedListsSolutions1

--- leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/sudoku/solver/demo01/SudokuSolverSolution1.java
+++ leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/sudoku/solver/demo01/SudokuSolverSolution1.java
@@ -1,15 +1,33 @@
 package org.frank.leetcode.array.sudoku.solver.demo01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode.cn/problems/sudoku-solver/description/
- * */
+ */
 public class SudokuSolverSolution1 {
     public void solveSudoku(char[][] board) {
-        int rows[][] = new int[9][9];
-        int columns[][] = new int[9][9];
-        int subBox[][][] = new int[3][3][9];
+        boolean rows[][] = new boolean[9][9];
+        boolean columns[][] = new boolean[9][9];
+        boolean subBox[][][] = new boolean[3][3][9];
         boolean valid = false;
-        
+        List<int[]> space = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                char c = board[i][j];
+                if(c == '.'){
+                    space.add(new int[]{i,j});
+                }else{
+                    int digit = c - '0' - 1;
+                    rows[i][digit] = false;
+                    columns[j][digit] = false;
+                    subBox[i/3][j/3][digit] = false;
+                }
+            }
+        }
+    }
+    private void dfs(char[][] board, int pos) {
         
     }
 }
