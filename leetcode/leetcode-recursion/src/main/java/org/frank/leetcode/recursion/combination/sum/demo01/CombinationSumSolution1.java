a7537c5cabfd86c802a01d5ced77e6ccb8bf07d4
fengandzhy <fengandzhy@gmail.com>
2023-11-22 22:02:42 +1300

    leetcode-linked_list MergeKSortedListsSolutions1

--- leetcode/leetcode-recursion/src/main/java/org/frank/leetcode/recursion/combination/sum/demo01/CombinationSumSolution1.java
+++ leetcode/leetcode-recursion/src/main/java/org/frank/leetcode/recursion/combination/sum/demo01/CombinationSumSolution1.java
@@ -4,7 +4,10 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
-
+/**
+ * https://leetcode.cn/problems/combination-sum/description/
+ * 
+ * */
 public class CombinationSumSolution1 {
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
@@ -27,8 +30,11 @@ public class CombinationSumSolution1 {
             lists.add(integers);
             return;
         }
+        // 重点理解这里从 begin 开始搜索的语意
         for(int i = begin; i<length; i++){
             deque.addLast(candidates[i]);
+            
+            // 注意：由于每一个元素可以重复使用，下一轮搜索的起点依然是 i，这里非常容易弄错
             dfs(candidates,i,length,target-candidates[i],lists,deque);
             deque.removeLast();
         }
