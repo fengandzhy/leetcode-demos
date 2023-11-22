1530ec8422fcba226f477faa17adc715a7a977ce
fengandzhy <fengandzhy@gmail.com>
2023-11-22 21:46:08 +1300

    leetcode-linked_list MergeKSortedListsSolutions1

--- leetcode/leetcode-recursion/src/main/java/org/frank/leetcode/recursion/combination/sum/demo01/CombinationSumSolution1.java
+++ leetcode/leetcode-recursion/src/main/java/org/frank/leetcode/recursion/combination/sum/demo01/CombinationSumSolution1.java
@@ -1,43 +1,36 @@
 package org.frank.leetcode.recursion.combination.sum.demo01;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class CombinationSumSolution1 {
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> lists = new ArrayList<>();
-        List<Integer> integers = new ArrayList<>();
-        nestMinus(candidates,target,lists,integers);
-        return null;
+        int length = candidates.length;
+        if(length == 0){
+            return lists;
+        }
+        Deque<Integer> deque = new ArrayDeque<>(length);
+        dfs(candidates,0,length,target,lists,deque);
+        return lists;
     }
     
-    private void nestMinus(int[] candidate, int target,List<List<Integer>> lists,List<Integer> integers){
+    private void dfs(int[] candidates, int begin, int length, int target, List<List<Integer>> lists, Deque<Integer> deque){
         if(target < 0){
             return;
         }
         if(target == 0){
-            
+            List<Integer> integers = new ArrayList<>(deque);
+            lists.add(integers);
+            return;
         }
-    }
-    
-    private boolean compareTwoList(List<Integer> l1, List<Integer> l2){
-        int size1 = l1.size();
-        int size2 = l2.size();
-        if(size1 == size2 && size1 > 0){
-            l1.sort((a,b) ->{
-                return a -b;
-            });
-            l2.sort((a,b) ->{
-                return a -b;
-            });
-            for(int i = 0; i< size1;i++){
-                if(l1.get(i) != l2.get(i)){
-                    return false;
-                }
-            }
-            return true;
+        for(int i = begin; i<length; i++){
+            deque.addFirst(candidates[i]);
+            dfs(candidates,begin,length,target-candidates[i],lists,deque);
+            deque.removeLast();
         }
-        return false;
-    }
+    }    
 }
