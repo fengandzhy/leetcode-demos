3dbc603539131343f1582794f40a698208f2e450
fengandzhy <fengandzhy@gmail.com>
2023-11-24 06:36:49 +1300

    leetcode-linked_list MergeKSortedListsSolutions1

--- leetcode/leetcode-dynamic_programming/src/main/java/org/frank/leetcode/dynamic/programming/climbing/stairs/demo01/ClimbingStairsSolution1.java
+++ leetcode/leetcode-dynamic_programming/src/main/java/org/frank/leetcode/dynamic/programming/climbing/stairs/demo01/ClimbingStairsSolution1.java
@@ -7,14 +7,25 @@ package org.frank.leetcode.dynamic.programming.climbing.stairs.demo01;
  * */
 public class ClimbingStairsSolution1 {    
     public int climbStairs(int n) {
-        if(n == 1){
-            return 1;
+//        if(n == 1 || n == 0){
+//            return 1;
+//        }
+//        if(n > 1){
+//            int a = n -1;
+//            int b = n -2;
+//            return climbStairs(a) + climbStairs(b);           
+//        }        
+//        return 1;
+        int[] dp = new int[n + 1];
+        if (n < 3)
+            return n;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+            System.out.println(dp[i]);
         }
-        if(n > 1){
-            int a = n -1;
-            int b = n -2;
-            return climbStairs(a) + climbStairs(b);           
-        }        
-        return 1;
+        return dp[n];
     }
 }
