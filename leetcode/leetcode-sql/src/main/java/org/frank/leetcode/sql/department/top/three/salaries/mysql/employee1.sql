c854e03fbd1a947f3bf80f9811539363945fafdb
fengandzhy <fengandzhy@gmail.com>
2023-11-26 10:42:03 +1300

    leetcode-sql tables

---
+++ leetcode/leetcode-sql/src/main/java/org/frank/leetcode/sql/department/top/three/salaries/mysql/employee1.sql
@@ -0,0 +1,7 @@
+INSERT INTO `employee1`(`id`, `name`, `salary`, `departmentId`) VALUES (1, 'Joe', 8500, 1);
+INSERT INTO `employee1`(`id`, `name`, `salary`, `departmentId`) VALUES (2, 'Henry', 8000, 2);
+INSERT INTO `employee1`(`id`, `name`, `salary`, `departmentId`) VALUES (3, 'Sam', 6000, 2);
+INSERT INTO `employee1`(`id`, `name`, `salary`, `departmentId`) VALUES (4, 'Max', 9000, 1);
+INSERT INTO `employee1`(`id`, `name`, `salary`, `departmentId`) VALUES (5, 'Janet', 6900, 1);
+INSERT INTO `employee1`(`id`, `name`, `salary`, `departmentId`) VALUES (6, 'Randy', 8500, 1);
+INSERT INTO `employee1`(`id`, `name`, `salary`, `departmentId`) VALUES (7, 'Will', 7000, 1);
