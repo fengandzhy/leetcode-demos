ff9f8654727fff1310be32cf1728bf4599d4d48a
fengandzhy <fengandzhy@gmail.com>
2023-11-27 22:25:04 +1300

    leetcode-sql tables

--- leetcode/leetcode-sql/src/main/java/org/frank/leetcode/sql/game/play/analysis/i/mysql/game-play-analysis-i.init.sql
+++ leetcode/leetcode-sql/src/main/java/org/frank/leetcode/sql/game/play/analysis/i/mysql/game-play-analysis-i.init.sql
@@ -5,3 +5,4 @@ Create table If Not Exists Activity (player_id int, device_id int, event_date da
     insert into Activity (player_id, device_id, event_date, games_played) values ('2', '3', '2017-06-25', '1');
     insert into Activity (player_id, device_id, event_date, games_played) values ('3', '1', '2016-03-02', '0');
     insert into Activity (player_id, device_id, event_date, games_played) values ('3', '4', '2018-07-03', '5');
+
