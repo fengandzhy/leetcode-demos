d138fea671457cc3064d3261b311f5e460e1acf9
fengandzhy <fengandzhy@gmail.com>
2023-12-05 07:29:28 +1300

    leetcode-sql tables

--- leetcode/leetcode-sql/src/main/java/org/frank/leetcode/sql/trips/and/users/mysql/trips-and-users.sql
+++ leetcode/leetcode-sql/src/main/java/org/frank/leetcode/sql/trips/and/users/mysql/trips-and-users.sql
@@ -13,3 +13,20 @@ WHERE
   AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
 GROUP BY
     request_at;
+
+-----------------------------------------------
+
+SELECT
+    a.request_at AS DAY,
+	ROUND( sum( CASE WHEN STATUS IN ( 'cancelled_by_driver', 'cancelled_by_client' ) THEN 1 ELSE 0 END ) / count( * ), 2 ) AS "Cancellation Rate"
+FROM
+    Trips a
+    LEFT JOIN Users b ON a.client_id = b.users_id
+    LEFT JOIN Users c ON a.driver_id = c.users_id
+WHERE
+    a.request_at >= "2013-10-01"
+  AND a.request_at <= "2013-10-03"
+  AND b.banned = 'No'
+  AND c.banned = 'No'
+GROUP BY
+    a.request_at
