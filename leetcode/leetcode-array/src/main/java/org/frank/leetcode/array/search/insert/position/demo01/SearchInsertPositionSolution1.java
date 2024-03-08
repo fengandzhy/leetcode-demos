a491ea703e579b7b997ff587e4b31b438fc95370
fengandzhy <fengandzhy@gmail.com>
2024-03-09 09:50:52 +1300

    format the code

--- leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/search/insert/position/demo01/SearchInsertPositionSolution1.java
+++ leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/search/insert/position/demo01/SearchInsertPositionSolution1.java
@@ -2,14 +2,13 @@ package org.frank.leetcode.array.search.insert.position.demo01;
 
 /**
  * https://leetcode.cn/problems/search-insert-position/
- * 
- * */
+ */
 public class SearchInsertPositionSolution1 {
     public int searchInsert(int[] nums, int target) {
         int index = 0;
-        if(nums!=null && nums.length>0){            
-            for(int i = 0; i< nums.length; i++){
-                if(nums[i] >= target){
+        if (nums != null && nums.length > 0) {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] >= target) {
                     return i;
                 }
             }
