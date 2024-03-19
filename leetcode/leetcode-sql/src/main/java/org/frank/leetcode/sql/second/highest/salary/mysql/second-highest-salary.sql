ccd71e79952e90eff993804efb279ba7f154c981
fengandzhy <fengandzhy@gmail.com>
2024-03-19 17:15:43 +1300

    SearchInsertPositionSolution3

--- leetcode/leetcode-sql/src/main/java/org/frank/leetcode/sql/second/highest/salary/mysql/second-highest-salary.sql
+++ leetcode/leetcode-sql/src/main/java/org/frank/leetcode/sql/second/highest/salary/mysql/second-highest-salary.sql
@@ -1,1 +1,8 @@
 select MAX(salary) as SecondHighestSalary from Employee where salary < (select MAX(salary) from Employee );
+
+SELECT (
+   SELECT DISTINCT Salary
+   FROM Employee
+   ORDER BY Salary DESC
+   LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
