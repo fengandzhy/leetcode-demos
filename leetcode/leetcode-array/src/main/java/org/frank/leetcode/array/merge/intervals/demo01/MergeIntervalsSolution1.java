7d346e3600c00274673b6c38a0eb87acdd433ab2
fengandzhy <fengandzhy@gmail.com>
2024-03-19 15:43:40 +1300

    SearchInsertPositionSolution3

---
+++ leetcode/leetcode-array/src/main/java/org/frank/leetcode/array/merge/intervals/demo01/MergeIntervalsSolution1.java
@@ -0,0 +1,11 @@
+package org.frank.leetcode.array.merge.intervals.demo01;
+
+/**
+ * https://leetcode.cn/problems/merge-intervals/
+ * */
+public class MergeIntervalsSolution1 {
+    public int[][] merge(int[][] intervals) {
+        
+        return null;
+    }
+}
