4800721c860a64b59db57e9147caf53327d7f9a7
fengandzhy <fengandzhy@gmail.com>
2024-04-30 09:27:39 +1200

    feature file

--- leetcode/leetcode-string/src/test/java/org/frank/cucumber/test/RunCucumberTest.java
+++ leetcode/leetcode-string/src/test/java/org/frank/cucumber/test/RunCucumberTest.java
@@ -3,7 +3,15 @@ package org.frank.cucumber.test;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-
+/**
+ * features: 指定Cucumber特性文件的位置，这里是src/test/resources/features目录。
+ * glue: 指定步骤定义代码的包名，这里是your_package，你需要将其替换为包含你的步骤定义类的实际包名。
+ * plugin: 配置Cucumber报告的插件，比如这里使用的pretty使得控制台输出更易读，html插件生成HTML格式的报告，并保存在target/cucumber-reports目录。
+ * monochrome: 设置为true以获得更清晰的控制台输出。
+ * strict: 设置为true使得在有未实现的步骤时测试失败。
+ * 
+ * 
+ * */
 @RunWith(Cucumber.class)
 // 配置Cucumber选项
 @CucumberOptions(
